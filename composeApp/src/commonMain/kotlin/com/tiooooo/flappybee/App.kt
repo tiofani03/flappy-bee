08dc8f9c01751059236ec3e1e00534c46fc9b9d5
Tio Fani <43690617+tiofani03@users.noreply.github.com>
2025-01-21 07:53:02 +0700

    feat: jumping logic (#2)

--- composeApp/src/commonMain/kotlin/com/tiooooo/flappybee/App.kt
+++ composeApp/src/commonMain/kotlin/com/tiooooo/flappybee/App.kt
@@ -1,8 +1,12 @@
 package com.tiooooo.flappybee
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,10 +14,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.withFrameMillis
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.tiooooo.flappybee.domain.Game
+import com.tiooooo.flappybee.domain.GameStatus
 import com.tiooooo.flappybee.util.ChewyFontFamily
 import flappybee.composeapp.generated.resources.Res
 import flappybee.composeapp.generated.resources.background
@@ -24,6 +40,23 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
+        var screenWidth by remember { mutableStateOf(0) }
+        var screenHeight by remember { mutableStateOf(0) }
+        var game by remember { mutableStateOf(Game()) }
+
+        LaunchedEffect(Unit) {
+            game.start()
+        }
+
+        LaunchedEffect(game.status) {
+            while (game.status == GameStatus.Started) {
+                withFrameMillis {
+                    game.updateGameProgress()
+                }
+            }
+        }
+
+
         Box(
             modifier = Modifier.fillMaxSize(),
         ) {
@@ -52,6 +85,54 @@ fun App() {
                 fontSize = MaterialTheme.typography.displaySmall.fontSize,
                 fontFamily = ChewyFontFamily(),
             )
+        }
+
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+                .onGloballyPositioned {
+                    val size = it.size
+                    if (screenWidth != size.width || screenHeight != size.height) {
+                        screenWidth = size.width
+                        screenHeight = size.height
+
+                        game = game.copy(
+                            screenWidth = size.width,
+                            screenHeight = size.height,
+                        )
+                    }
+                }
+                .clickable {
+                    if (game.status == GameStatus.Started) {
+                        game.jump()
+                    }
+                }
+        ) {
+            drawCircle(
+                color = Color.Blue,
+                radius = game.bee.radius,
+                center = Offset(
+                    x = game.bee.x,
+                    y = game.bee.y,
+                )
+            )
+        }
+
+        if (game.status == GameStatus.Over) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = Color.Black.copy(alpha = 0.5f)),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "Game Over!",
+                    color = Color.White,
+                    fontSize = MaterialTheme.typography.displaySmall.fontSize,
+                    fontFamily = ChewyFontFamily(),
+                )
+            }
         }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/tiooooo/flappybee/domain/Bee.kt
@@ -0,0 +1,7 @@
+package com.tiooooo.flappybee.domain
+
+data class Bee(
+    val x: Float,
+    val y: Float,
+    val radius: Float = 30f
+)
