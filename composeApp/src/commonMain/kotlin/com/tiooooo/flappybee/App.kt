29a0d2d977cfb3bbba011f8db60b4318179e30d8
tiofani <tiofani03@gmail.com>
2025-01-21 07:02:31 +0700

    setup background

--- composeApp/src/commonMain/kotlin/com/tiooooo/flappybee/App.kt
+++ composeApp/src/commonMain/kotlin/com/tiooooo/flappybee/App.kt
@@ -1,13 +1,57 @@
 package com.tiooooo.flappybee
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.tiooooo.flappybee.util.ChewyFontFamily
+import flappybee.composeapp.generated.resources.Res
+import flappybee.composeapp.generated.resources.background
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme{
-
+    MaterialTheme {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(Res.drawable.background),
+                contentDescription = "backgroundImage",
+                contentScale = ContentScale.Crop,
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(48.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(
+                text = "BEST: 0",
+                fontWeight = FontWeight.Bold,
+                fontSize = MaterialTheme.typography.displaySmall.fontSize,
+                fontFamily = ChewyFontFamily(),
+            )
+            Text(
+                text = "0",
+                fontWeight = FontWeight.Bold,
+                fontSize = MaterialTheme.typography.displaySmall.fontSize,
+                fontFamily = ChewyFontFamily(),
+            )
+        }
     }
 }
